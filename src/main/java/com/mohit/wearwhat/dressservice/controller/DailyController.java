d78605d57717e7ad1148b2437f2f0e6ebf607e2f
Mohit Raj <Mohit.Raj@Honeywell.com>
2020-01-28 19:29:53 +0530

    testing file write operation

--- src/main/java/com/mohit/wearwhat/dressservice/controller/DailyController.java
+++ src/main/java/com/mohit/wearwhat/dressservice/controller/DailyController.java
@@ -1,5 +1,7 @@
 package com.mohit.wearwhat.dressservice.controller;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -18,6 +20,34 @@ public class DailyController {
 		String content = null;
 		try {
 			content = new String ( Files.readAllBytes(resourceDirectory.resolve("week1.txt")) );
+		} catch(Exception ex) {
+			System.out.println(ex.getMessage());
+			return ResponseEntity.notFound().build();
+		}
+	    return ResponseEntity.ok().body(content);
+	}
+	
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public ResponseEntity<Object> register() {
+		Path resourceDirectory = Paths.get("src","main","resources", "data");
+		File file = new File(resourceDirectory.toFile().getAbsolutePath() + "/newFile.txt");
+		
+		String content = null;
+		try {
+			if (file.createNewFile())
+			{
+			    System.out.println("File is created!");
+			  
+			} else {
+			    System.out.println("File already exists.");
+			}
+			
+			//Write Content
+		    FileWriter writer = new FileWriter(file);
+		    writer.write("new dress registered " + System.currentTimeMillis());
+		    writer.close();
+		    
+		    content = new String ( Files.readAllBytes(resourceDirectory.resolve("newFile.txt")) );
 		} catch(Exception ex) {
 			System.out.println(ex.getMessage());
 			return ResponseEntity.notFound().build();
